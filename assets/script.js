177d8f30c103891ee95ee3afe098709dac54b5c1
unknown <adamxabulkheir@gmail.com>
2023-04-03 19:36:10 -0400

    added more code

---
+++ assets/script.js
@@ -0,0 +1,55 @@
+const Quizquestions= [
+    {
+        q: "What does HTML stand for?",
+        a: {
+            1: "Hottub Time Machine Lore"
+            2: "Hotdogs Took My Lover"
+            3: "Hyperloop Train Movement Logic"
+            4: "Hyper Text Markup Language"
+        
+        },
+        correctAns: "Hyper Text Markup Language"
+    },
+    {
+        q: "What does CSS stand for?",
+        a: {
+            1: "Cascade Silver Solution"
+            2: "Costly Sandwich Service"
+            3: "Cascading Style Sheets"
+            4: "Carpool Student Survey"
+        },
+        correctAns: "Cascading Style Sheets"
+    },
+    {
+        q: "What does Javascript do?",
+        a: {
+            1: "Handle logic letting users interact with a site"
+            2: "Serve coffee while you wait for the site to load"
+            3: "Colors your webpage"
+            4: "Absolutely nothing at all, its imaginary"
+        },
+        correctAns: "Handle logic letting users interact with a site"
+    },
+    {
+        q: 
+        "Which Git command uploads changes made to a repository?",
+        a: {
+            1: "Git Punch"
+            2: "Git Commit"
+            3: "Git Gud"
+            4: "Git Pull"
+        },
+        correctAns: "Git Commit"
+    },
+    {
+        q: "What is a CLI?",
+        a: {
+            1: "Comically Large Iguana"
+            2: "Command Line Interface"
+            3: "Command Line Interweb"
+            4: "Coast Line Internet"
+        },
+        correctAns: "Command Line Interface"
+    }
+];
+
